c1de96462dd9160f56085f709822df3cb16c693f
fannyguthmann <fanny.guthmann@post.idc.ac.il>
2023-08-07 11:56:40 +0300

    Added comments to document-state/contract_storage_state.rs

--- src/state/contract_storage_state.rs
+++ src/state/contract_storage_state.rs
@@ -9,16 +9,21 @@ use crate::{
 use cairo_vm::felt::Felt252;
 use std::collections::HashSet;
 
+/// Represents the storage state of a contract, keeping track of read values and accessed keys.
 #[derive(Debug)]
 pub(crate) struct ContractStorageState<'a, S: StateReader> {
+    /// Cached state reference for reading and writing contract storage.
     pub(crate) state: &'a mut CachedState<S>,
+    /// Address of the contract whose storage is being managed.
     pub(crate) contract_address: Address,
     /// Maintain all read request values in chronological order
     pub(crate) read_values: Vec<Felt252>,
+    /// Set of keys (ClassHash) that have been accessed (either read or written to).
     pub(crate) accessed_keys: HashSet<ClassHash>,
 }
 
 impl<'a, S: StateReader> ContractStorageState<'a, S> {
+    /// Creates a new ContractStorageState instance for a given contract address.
     pub(crate) fn new(state: &'a mut CachedState<S>, contract_address: Address) -> Self {
         Self {
             state,
@@ -28,6 +33,8 @@ impl<'a, S: StateReader> ContractStorageState<'a, S> {
         }
     }
 
+    /// Reads a value from the contract's storage for a given key (address).
+    /// Records the read value and adds the key to the set of accessed keys.
     pub(crate) fn read(&mut self, address: &ClassHash) -> Result<Felt252, StateError> {
         self.accessed_keys.insert(*address);
         let value = self
@@ -38,6 +45,8 @@ impl<'a, S: StateReader> ContractStorageState<'a, S> {
         Ok(value)
     }
 
+    /// Writes a value to the contract's storage for a given key (address).
+    /// Adds the key to the set of accessed keys.
     pub(crate) fn write(&mut self, address: &ClassHash, value: Felt252) {
         self.accessed_keys.insert(*address);
         self.state
